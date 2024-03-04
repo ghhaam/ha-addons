70e4ce0b52b1b1b0d291b17d9e18aeb04295ae13
ghhaam <124436684+ghhaam@users.noreply.github.com>
2024-03-04 19:36:51 +0100

    Update oled.py

--- oled/rootfs/usr/bin/oled.py
+++ oled/rootfs/usr/bin/oled.py
@@ -37,7 +37,9 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe(str(args.mqtt_message_lisener))
+    #client.subscribe(str(args.mqtt_message_lisener))
+    client.subscribe(oled/message)
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
