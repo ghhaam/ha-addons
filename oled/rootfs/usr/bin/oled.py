7748532e2af81d567ef7963a274ecf3b4826ec69
ghhaam <124436684+ghhaam@users.noreply.github.com>
2024-03-04 19:21:36 +0100

    Update oled.py

--- oled/rootfs/usr/bin/oled.py
+++ oled/rootfs/usr/bin/oled.py
@@ -65,9 +65,9 @@ device = globals()[DISPLAY_TYPE](interface, rotate=DISPLAY_ROTATE)
 device.contrast(50)
 
 #fnt = ImageFont.truetype("/usr/bin/SF-Compact.ttf", int(args.message_font_size))
-fnt = ImageFont.truetype("/usr/bin/SF-Compact.ttf", 40)
+fnt = ImageFont.truetype("/usr/bin/SF-Compact.ttf", 40, encoding="unic")
 #fnt_unit = ImageFont.truetype("/usr/bin/SF-Compact.ttf", int(args.message_unit_font_size))
-fnt_unit = ImageFont.truetype("/usr/bin/SF-Compact.ttf", 20)
+fnt_unit = ImageFont.truetype("/usr/bin/SF-Compact.ttf", 20, encoding="unic")
 
 # Draw some text
 with canvas(device) as draw:
